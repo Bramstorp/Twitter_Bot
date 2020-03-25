53db5924636e029175e0a6903795a8e9d3fb8fec
Mathias V. Bramstorp <50810755+Bramstorp@users.noreply.github.com>
2020-03-25 20:56:54 +0100

    Add files via upload

---
+++ Twitter_Bot.py
@@ -0,0 +1,44 @@
+import tweepy
+import time
+
+# Get this key from Twitter Api u can search u get the api as a Twitter Developer
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+user = api.me()
+
+# This is the u check if u go over the Rate Limit of Twitters API
+def limit_handle(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.RateLimitError:
+            time.sleep(1000)
+
+#Keyword u search for
+search_string = "python"
+
+# How many tweets u want to like etc.
+number_of_tweets = 2
+
+# Search from search string and like th number amount of tweets set in number_of_tweets
+for tweet in tweepy.Cursor(api.search, search_string).items(number_of_tweets):
+    try:
+        tweet.favorite()
+        print("The Tweet has been liked")
+    except tweepy.TweepError as err:
+        print(err.reason)
+    except StopIteration:
+        break
+
+
+#Follow Back Bot
+for follower in limit_handle(tweepy.Cursor(api.followers).items()):
+    if follower.name == "FOLLOWER NAME":
+        follower.follow()
+        print("Person has been followed")
+        break
+
+print (user.name) # Prints UserName
+print (user.followers_count) # Prints Follow Count
