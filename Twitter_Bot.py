21c2073017ec8cd1dd86248e3158730d20a45437
Mathias V. Bramstorp <50810755+Bramstorp@users.noreply.github.com>
2020-03-28 16:37:20 +0100

    Update Twitter_Bot.py

--- Twitter_Bot.py
+++ Twitter_Bot.py
@@ -45,5 +45,14 @@ for follower in limit_handle(tweepy.Cursor(api.followers).items()):
         print("Person has been followed")
         break
 
+# Shows each followers of yours by name
+for follower in tweepy.Cursor(api.followers).items():
+    print(follower.name)
+
+# This is for the twitter timeline
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
+
 print (user.name) # Prints UserName
 print (user.followers_count) # Prints Follow Count
